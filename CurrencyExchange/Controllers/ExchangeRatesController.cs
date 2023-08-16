894b5706c754afc4dd04208758b938a9e0411aaf
Ryan Vella <ryanvella7@gmail.com>
2023-08-16 23:01:05 +0200

    added exchange rates controller

---
+++ CurrencyExchange/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,33 @@
+﻿using CurrencyExchange.Interfaces;
+using CurrencyExchange.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyExchange.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExchangeRatesController : ControllerBase
+    {
+        private readonly IExchangeRateProviderService _exchangeRateService;
+
+        public ExchangeRatesController(IExchangeRateProviderService exchangeRateService)
+        {
+            _exchangeRateService = exchangeRateService;
+        }
+
+        [HttpGet("latest")]
+        public async Task<ActionResult<decimal>> GetLatestExchangeRate(ExchangeRateModel exchangeRateModel)
+        {
+            try
+            {
+
+                decimal exchangeRate = await _exchangeRateService.GetLatestExchangeRateAsync(exchangeRateModel);
+                return Ok(exchangeRate);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
