2f7385d264592c6d808b7fc5d576568493c28924
Ryan Vella <ryanvella7@gmail.com>
2023-08-16 16:43:27 +0200

    created exchange rates DTO Models

---
+++ CurrencyExchange/Models/DTOs/ExchangeRateDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CurrencyExchange.Models.DTOs
+{
+    public class ExchangeRateDTO
+    {
+        public int Id { get; set; }
+        public string? SourceCurrency { get; set; }
+        public string? TargetCurrency { get; set; }
+        [Column(TypeName = "decimal(18,6)")]
+        public decimal Rate { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
