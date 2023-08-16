5e45ec7e463becfbb9f14b807b35572f392fd2d3
Ryan Vella <ryanvella7@gmail.com>
2023-08-16 16:51:17 +0200

    added db ef files and migrated

---
+++ CurrencyExchange/DbContexts/CurrencyExchangeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CurrencyExchange.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyExchange.DbContexts
+{
+    public class CurrencyExchangeDbContext : DbContext
+{
+    public CurrencyExchangeDbContext(DbContextOptions<CurrencyExchangeDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<CurrencyExchangeTradeDTO>? CurrencyExchangeTrades { get; set; }
+    public DbSet<ExchangeRateDTO>? ExchangeRates { get; set; }
+}
+}
---
+++ CurrencyExchange/Migrations/20230816144230_InitialCreateDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyExchange.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrencyExchangeTrades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,6)", nullable: false),
+                    SourceCurrency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TargetCurrency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyExchangeTrades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourceCurrency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TargetCurrency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rate = table.Column<decimal>(type: "decimal(18,6)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyExchangeTrades");
+
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+        }
+    }
+}
